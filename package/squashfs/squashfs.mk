3be202e95a1c26f69e2f3b8487fb042a61deae08
Steven Noble <snoble@sonn.com>
2025-11-20 18:54:05 +0000

    chore: Update packages to work with Bookworm
    Signed-off-by: Steven Noble <snoble@sonn.com>

--- package/squashfs/squashfs.mk
+++ package/squashfs/squashfs.mk
@@ -1,4 +1,4 @@
-SQUASHFS_VERSION=4.4
+SQUASHFS_VERSION=4.2
 SQUASHFS_SOURCE=squashfs$(SQUASHFS_VERSION).tar.gz
 SQUASHFS_SITE=http://downloads.sourceforge.net/projct/squashfs/squashfs/squashfs$(SQUASHFS_VERSION)
 
